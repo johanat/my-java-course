ec768c76d5bf2014e726a97fbd626150bca757f4
Darwin Córdova <darwin.cordova@nexplore.com>
2022-09-08 18:14:59 +0200

    Ejemplo con clase Person

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Soy Darwin");
+
+        Person johana;
+        Person darwin;
+
+        johana = new Person();
+        darwin = new Person();
+
+        johana.name = "Johana";
+        johana.age = 28;
+        johana.weight = 70.0F;
+
+        darwin.name = "Darwin";
+        darwin.age = 34;
+        darwin.weight = 63.0F;
+
+        johana.printInfo();
+        darwin.printInfo();
+
+    }
+}
+
+class Person {
+
+    String name;
+    int age;
+    float weight; //Peso
+
+    Person(){}
+
+    void eat() {
+        System.out.println("Eating...");
+    }
+
+    void sleep() {
+        System.out.println("Sleeping...");
+    }
+
+    void printInfo() {
+        System.out.println("My name is " + name + ", I'm "+ age + ", and my weight is " + weight);
     }
 }
