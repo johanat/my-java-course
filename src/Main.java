b578ab2fc9c469c8795a954a93bc7b35ba4323c9
Johana <johanatonguino94@gmail.com>
2022-09-13 17:11:15 +0200

    ejercicio 2

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,54 @@
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Car myCar = new Car(); // myCar es una instancia de car
 
-        String color;
-        String model;
+    public static void main(String[] args) {
+
+        ArrayList<Person> people = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce the color at car  ");
-        color = scanner.nextLine();
-        myCar.color = color;
+        int number;
 
-        System.out.println("Introduce the  car model");
-        model = scanner.nextLine();
-        myCar.model = model;
-        myCar.printInfo();
+        System.out.println("Introduce a number from 1 to 5");
+        number = scanner.nextInt();
+        if (number <= 5) {
+            for (int i = 0; i < number; i++) {
+                System.out.println("Introduce your name");
+                String name = scanner.next();
+                System.out.println("Introduce your age");
+                int age = scanner.nextInt();
+                System.out.println("Introduce your weight");
+                float weight = scanner.nextFloat();
 
+                Person person1 = new Person(name, age, weight); // Creando una instancia, pasa parametros al contructor.
+                people.add(person1);
+            }
+        } else {
+            System.out.println("Number invalid");
+        }
+
+        for (int j = 0; j < people.size(); j++) {
+            System.out.println("persona" + j);
+            people.get(j).printInfo();
+        }
     }
-
 }
 
-class Car {
-    String color;
-    String model;
+class Person {
+    String name;
+    int age;
+    float weight;
 
-    Car() {
-    }
-
-    void start() {
-        System.out.println("staring");
-    }
-
-    void stop() {
-        System.out.println("stopping");
+    Person(String name, int age, float weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
     }
 
     void printInfo() {
-        System.out.println("color = " + color);
-        System.out.println("model = " + model);
-    }
+        System.out.println("My name is " + name.toUpperCase() + " I am age  " + age + " and my weight is " + weight);
 
+    }
 }
