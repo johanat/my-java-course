64f906cb5826a1cabed6c4391bf7c18434a4170e
Johana <johanatonguino94@gmail.com>
2022-09-14 18:44:05 +0200

    ejercicio 4

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,58 @@
-import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Person> people = new ArrayList<>();
+        int num1;
+        int num2;
 
         Scanner scanner = new Scanner(System.in);
-        int number;
-        int index;
-        System.out.println("Introduce a number from 1 to 5");
-        number = scanner.nextInt();
-        if (number <= 5) {
-            for (int i = 0; i < number; i++) {
-                System.out.println("Introduce your name");
-                String name = scanner.next();
-                System.out.println("Introduce your age");
-                int age = scanner.nextInt();
-                System.out.println("Introduce your weight");
-                float weight = scanner.nextFloat();
+        System.out.println("Introduce the first number");
+        num1 = scanner.nextInt();
+        System.out.println("Introduce the second number");
+        num2 = scanner.nextInt();
+        Calculator operation = new Calculator();
 
-                Person person1 = new Person(name, age, weight); // Creando una instancia, pasa parametros al contructor.
-                people.add(person1);
-            }
-        } else {
-            System.out.println("Number invalid");
-        }
+        System.out.println("suma "+operation.sum(num1,num2));
+        System.out.println("resta "+operation.subtract(num1,num2));
+        System.out.println("multiplicacion "+operation.multiply(num1,num2));
+        System.out.println("division "+operation.divide(num1,num2));
 
-        printPeopleArray(people);
-
-        System.out.println("\n Which user do you want to modify?, introduce a index");
-        index = scanner.nextInt();
-
-        System.out.println("Introduce your name");
-        people.get(index).name = scanner.next();
-        System.out.println("Introduce your age");
-        people.get(index).age = scanner.nextInt();
-        System.out.println("Introduce your weight");
-        people.get(index).weight = scanner.nextFloat();
-
-        printPeopleArray(people);
-
-    }
-    static void printPeopleArray(ArrayList<Person> people) {
-        for (int k = 0; k < people.size(); k++) {
-            System.out.println("persona" + k);
-            people.get(k).printInfo();
-        }
     }
 }
 
-class Person {
-    String name;
-    int age;
-    float weight;
 
-    Person(String name, int age, float weight) {
-        this.name = name;
-        this.age = age;
-        this.weight = weight;
+class Calculator {
+
+    Calculator() {
+
     }
 
-    void printInfo() {
-        System.out.println("\nMy name is " + name.toUpperCase() + " I am age  " + age + " and my weight is " + weight);
+    int sum(int num1, int num2) {
+        int sumR;
+        sumR = (num1+num2);
+
+        return sumR;
+    }
+
+    int subtract(int num1, int num2) {
+        int subtractR;
+        subtractR = num1-num2;
+        return subtractR;
+    }
+
+    int multiply(int num1, int num2) {
+        int multiplyR;
+        multiplyR = num1*num2;
+        return multiplyR;
+    }
+
+    double divide(int num1, int num2) {
+        double divieR;
+        divieR =num1*1f / num2;
+        return divieR;
     }
 }
+
+
