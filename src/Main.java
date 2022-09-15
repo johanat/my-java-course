c668df02d18bbd44484dc24622cc02e4fb02e4c4
Johana <johanatonguino94@gmail.com>
2022-09-15 21:25:10 +0200

    ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,70 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        int num1;
-        int num2;
+        Person user = new Person();
+        user.name = "Johana";
+        user.age = 28;
+        user.weight = 70.1f;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce the first number");
-        num1 = scanner.nextInt();
-        System.out.println("Introduce the second number");
-        num2 = scanner.nextInt();
-        Calculator operation = new Calculator();
+        System.out.println("Introduce the number of calculators");
+        int numberCalculator = scanner.nextInt();
 
-        System.out.println("suma "+operation.sum(num1,num2));
-        System.out.println("resta "+operation.subtract(num1,num2));
-        System.out.println("multiplicacion "+operation.multiply(num1,num2));
-        System.out.println("division "+operation.divide(num1,num2));
+        for (int i = 1; i <= numberCalculator; i++) {
+            Calculator calculatorNew = new Calculator(); // creando un objeto o una instacia de calculator
+            calculatorNew.name = "name" + i; // guardar datos en un string accediendo a el objeto
+            user.calculators.add(calculatorNew); // guardando cada elemento en el array calculatorNew
+        }
 
+        System.out.println("Introduce the number of the calculator that want print");
+        String numberPrintCalculator = scanner.next();
+
+        for (int j = 0; j < user.calculators.size(); j++) {
+            if (user.calculators.get(j).name.endsWith(numberPrintCalculator)) {
+                user.calculators.get(j).printInfo();
+            }
+        }
     }
 }
 
+class Person {
+    String name;
+    int age;
+    float weight;
+
+    ArrayList<Calculator> calculators = new ArrayList<>(); // declaracion de una lista de array y su parametro es (arraylista) array..
+
+
+    Person() {}
+
+    void eat() {
+        System.out.println("Eating...");
+    }
+
+    void sleep() {
+        System.out.println("Sleeping...");
+    }
+
+    void printInfo() {
+        System.out.println("My name is " + name + ", I'm " + age + ", and my weight is " + weight);
+    }
+}
 
 class Calculator {
+    String name;
 
     Calculator() {
 
     }
 
-    int sum(int num1, int num2) {
-        int sumR;
-        sumR = (num1+num2);
+    void printInfo() {
+        System.out.println("Mi nombre es" + name);
 
-        return sumR;
-    }
-
-    int subtract(int num1, int num2) {
-        int subtractR;
-        subtractR = num1-num2;
-        return subtractR;
-    }
-
-    int multiply(int num1, int num2) {
-        int multiplyR;
-        multiplyR = num1*num2;
-        return multiplyR;
-    }
-
-    double divide(int num1, int num2) {
-        double divieR;
-        divieR =num1*1f / num2;
-        return divieR;
     }
 }
+
 
 
