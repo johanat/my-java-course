e1034b406e22fbda471b1b1d7c26170692d08171
Johana <johanatonguino94@gmail.com>
2022-09-20 16:45:55 +0200

    ejercicio 6

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
         user.name = "Johana";
         user.age = 28;
         user.weight = 70.1f;
+        int k;
+        int counter = 0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduce the number of calculators");
@@ -15,20 +17,41 @@ public class Main {
 
         for (int i = 1; i <= numberCalculator; i++) {
             Calculator calculatorNew = new Calculator(); // creando un objeto o una instacia de calculator
-            calculatorNew.name = "name" + i; // guardar datos en un string accediendo a el objeto
+            calculatorNew.name = "name " + i; // guardar datos en un string accediendo a el objeto
             user.calculators.add(calculatorNew); // guardando cada elemento en el array calculatorNew
         }
 
-        System.out.println("Introduce the number of the calculator that want print");
-        String numberPrintCalculator = scanner.next();
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println(" 1. Add a calculator\n 2.Remove a calculator");
+        int numberUser = scanner.nextInt();
 
-        for (int j = 0; j < user.calculators.size(); j++) {
-            if (user.calculators.get(j).name.endsWith(numberPrintCalculator)) {
-                user.calculators.get(j).printInfo();
+        if (numberUser == 1) {
+            Calculator calculatorNewAdd = new Calculator(); // creando un objeto o una instacia de calculator
+            System.out.println("Introduce the name of calculators");
+            calculatorNewAdd.name = scanner.next();
+            user.calculators.add(calculatorNewAdd);
+        } else if (numberUser == 2) {
+            System.out.println("Enter the name of the calculator you want to remove ");
+            String nameRemove = scanner1.nextLine();
+            boolean removed = false;
+            for (k = 0; k < user.calculators.size(); k++) {
+                if (user.calculators.get(k).name.equals(nameRemove)) {
+                    user.calculators.remove(k);
+                    removed = true;
+                }
             }
+            if (removed) {
+                System.out.println("It has been deleted " + "(" + nameRemove + ")");
+            } else {
+                System.out.println("The calculator not exit");
+            }
+        }
+        for (int r = 0; r < user.calculators.size(); r++) {
+            user.calculators.get(r).printInfo();
         }
     }
 }
+
 
 class Person {
     String name;
@@ -37,8 +60,8 @@ class Person {
 
     ArrayList<Calculator> calculators = new ArrayList<>(); // declaracion de una lista de array y su parametro es (arraylista) array..
 
-
-    Person() {}
+    Person() {
+    }
 
     void eat() {
         System.out.println("Eating...");
@@ -61,10 +84,12 @@ class Calculator {
     }
 
     void printInfo() {
-        System.out.println("Mi nombre es" + name);
+        System.out.println("Mi nombre es " + name);
 
     }
 }
+
+
 
 
 
