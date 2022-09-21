b0bcbdcd874b0d13c26bd7c677bb81225809040b
Johana <johanatonguino94@gmail.com>
2022-09-21 18:13:06 +0200

    ejercicio 7

--- src/Main.java
+++ src/Main.java
@@ -3,93 +3,48 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        Person user = new Person();
-        user.name = "Johana";
-        user.age = 28;
-        user.weight = 70.1f;
-        int k;
-        int counter = 0;
 
+        ArrayList<String> wordsArray = new ArrayList<>(); // instanciando o creando un array
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce the number of calculators");
-        int numberCalculator = scanner.nextInt();
-
-        for (int i = 1; i <= numberCalculator; i++) {
-            Calculator calculatorNew = new Calculator(); // creando un objeto o una instacia de calculator
-            calculatorNew.name = "name " + i; // guardar datos en un string accediendo a el objeto
-            user.calculators.add(calculatorNew); // guardando cada elemento en el array calculatorNew
+        System.out.println("Introduce 10 words ");
+        for (int i = 0; i < 10; i++) {
+            String words = scanner.next();
+            wordsArray.add(words);
         }
-
+        System.out.println(" 1. Print the words that starts with a given \n 2. Check if a word be in the array");
         Scanner scanner1 = new Scanner(System.in);
-        System.out.println(" 1. Add a calculator\n 2.Remove a calculator");
-        int numberUser = scanner.nextInt();
-
+        int numberUser = scanner1.nextInt();
+        boolean same = false;
+        String wordCompared = null;
         if (numberUser == 1) {
-            Calculator calculatorNewAdd = new Calculator(); // creando un objeto o una instacia de calculator
-            System.out.println("Introduce the name of calculators");
-            calculatorNewAdd.name = scanner.next();
-            user.calculators.add(calculatorNewAdd);
-        } else if (numberUser == 2) {
-            System.out.println("Enter the name of the calculator you want to remove ");
-            String nameRemove = scanner1.nextLine();
-            boolean removed = false;
-            for (k = 0; k < user.calculators.size(); k++) {
-                if (user.calculators.get(k).name.equals(nameRemove)) {
-                    user.calculators.remove(k);
-                    removed = true;
+            System.out.println("Introduce a word");
+            String wordPrefix = scanner1.next();
+            for (int j = 0; j < wordsArray.size(); j++) {
+                if (wordsArray.get(j).startsWith(wordPrefix)) {
+                    System.out.println(wordsArray.get(j));
                 }
             }
-            if (removed) {
-                System.out.println("It has been deleted " + "(" + nameRemove + ")");
+        }
+        if (numberUser == 2) {
+            System.out.println("Introduce a word");
+            String wordCompare = scanner1.next();
+            for (int k = 0; k < wordsArray.size(); k++) {
+                if (wordsArray.get(k).equals(wordCompare)) {
+                    wordCompared = wordsArray.get(k);
+                    same = true;
+                }
+            }
+            if (same) {
+                System.out.println("Yes, the word exists  " + "(" + wordCompared + ") ^_^");
             } else {
-                System.out.println("The calculator not exit");
+                System.out.println("No, the word  doesn't exist -_-");
             }
         }
-        for (int r = 0; r < user.calculators.size(); r++) {
-            user.calculators.get(r).printInfo();
-        }
     }
+
 }
-
-
-class Person {
-    String name;
-    int age;
-    float weight;
-
-    ArrayList<Calculator> calculators = new ArrayList<>(); // declaracion de una lista de array y su parametro es (arraylista) array..
-
-    Person() {
-    }
-
-    void eat() {
-        System.out.println("Eating...");
-    }
-
-    void sleep() {
-        System.out.println("Sleeping...");
-    }
-
-    void printInfo() {
-        System.out.println("My name is " + name + ", I'm " + age + ", and my weight is " + weight);
-    }
-}
-
-class Calculator {
-    String name;
-
-    Calculator() {
-
-    }
-
-    void printInfo() {
-        System.out.println("Mi nombre es " + name);
-
-    }
-}
-
-
 
 
 
