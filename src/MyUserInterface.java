2a578eadaf04a534f7cfa13aebfdae120a767448
Darwin Córdova <darwin.cordova@nexplore.com>
2022-09-29 20:17:13 +0200

    Calculator starting point

--- src/MyUserInterface.java
+++ src/MyUserInterface.java
@@ -7,6 +7,19 @@ import java.awt.event.ActionListener;
 public class MyUserInterface extends JFrame{
 
     private JPanel mainPanel;
+    private JPanel row1;
+    private JPanel row2;
+    private JPanel row3;
+    private JPanel row4;
+    private JPanel row5;
+    private JTextField textField1;
+    private JButton btn0;
+    private JButton btn1;
+    private JButton btn2;
+    private JButton a3Button;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
     private JTextField txtInput;
     private JButton btnClick;
     private JLabel myLabel;
@@ -16,13 +29,13 @@ public class MyUserInterface extends JFrame{
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.setSize(new Dimension(600, 400));
-        btnClick.addActionListener(new ActionListener() {
+        btn0.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //Read the input text
-                String myText = txtInput.getText();
+                /*String myText = txtInput.getText();
                 myLabel.setText(myText);
-                txtInput.setText("");
+                txtInput.setText("");*/
             }
         });
     }
