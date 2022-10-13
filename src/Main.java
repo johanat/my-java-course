14a039b0c0d1b415db61a36c6841a0c71fc7c800
Johana <johanatonguino94@gmail.com>
2022-10-13 19:34:02 +0200

    Create, read and write a file

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,43 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Person johana = new Person("Johana", 5);
-        Person darwin = new Person("Darwin", 35);
+        try {
 
-        johana.eat();
+            //Create file
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile() == true) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+
+            //Write to file
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write("This is a test!");
+            myWriter.append("\nEsto es un añaadido");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+
+            //Read file
+            String text = "";
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine() == true) {
+                String line = myReader.nextLine();
+                text += line;
+            }
+
+            System.out.println("The text is = " + text);
+            myReader.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
 
