810afe573d6ce6d4a774417f35f1f082e43d50be
Johana <johanatonguino94@gmail.com>
2022-10-13 19:00:03 +0200

    ejercicio users manager

--- src/MyUserInterface.java
+++ src/MyUserInterface.java
@@ -3,63 +3,90 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 
-public class MyUserInterface extends JFrame  {
-
+public class MyUserInterface extends JFrame {
 
     private JPanel mainPanel;
-    private JTextField Height;
-    private JTextField weight;
-    private JButton IMCButton;
-    private JLabel resultado;
-    private JButton borrador;
+    private JTextField name1;
+    private JTextField edad;
+    private JTextField peso;
+    private JRadioButton buttonAutorizacion;
+    private JButton addUserButton;
+    private JPanel tituloGeneral;
+    private JLabel print;
+    ArrayList<Persona> peopleUsuare = new ArrayList<>(); // (variable global) lo ven todos y (variables locales)  debe estar dentro de un metodo y sera manejados solo los atributos de dicho metodo  ejemplo  MyUserInterface es un metodo mas si quieres que solo este metodo vea este array debes ponerlo dentro.
 
 
     MyUserInterface() {
+
+
         mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
-        this.setSize(new Dimension(500, 300));
+        this.setSize(new Dimension(500, 600));
+
+        addUserButton.addActionListener(new ActionListener() {
 
 
-        Height.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
 
+                String nameUsure = name1.getText();
+                String age = edad.getText();
+                String weight = peso.getText();
+                boolean autorizacionR = buttonAutorizacion.isSelected();
+
+                Persona person = new Persona(name1.getText(), Integer.parseInt(age), Float.parseFloat(weight), buttonAutorizacion.isSelected());
+                peopleUsuare.add(person);
+
+                String general = "";
+
+                for (int i = 0; i < peopleUsuare.size(); i++) {
+                    String nameC = peopleUsuare.get(i).name;
+                    String ageC = String.valueOf(peopleUsuare.get(i).age);
+                    String weightC = Float.toString(peopleUsuare.get(i).weight);
+                    boolean autorizacionC = peopleUsuare.get(i).autorizacion;
+
+                    general += "<html> Name: " + nameC + "<br/>" +
+                            " Age: " + ageC + "<br/>" +
+                            " Weight: " + weightC + "<br/>" +
+                            " Autorized: " + autorizacionC + "<br/>" +
+                            "________________" + "<br/>";
+
+                    print.setText(general);
+                }
+                name1.setText("");
+                edad.setText("");
+                peso.setText("");
+
             }
+
         });
-
-
-        weight.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        IMCButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                String peso = weight.getText();
-                float  numberWeight = Float.parseFloat(peso);
-
-
-                String altura = Height.getText();
-                float numberHeight = Float.parseFloat(altura);
-                float valor = numberHeight * numberHeight;
-
-                float resultImc = (numberWeight/valor);
-
-                String finaly = String.valueOf(resultImc);
-                resultado.setText("IMC = "+finaly);
-
-            }
-        });
-
     }
+
+
+    class Persona {
+        String name;
+        int age;
+        float weight;
+        boolean autorizacion;
+
+        Persona(String name, int age, float weight, boolean autorizacion) {
+
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.autorizacion = autorizacion;
+
+
+        }
+    }
+
+    ;
+
 
     public static void main(String[] args) {
         JFrame frame = new MyUserInterface();
