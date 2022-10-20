6691df815e173a157d01aaa09c720c8fd86d9850
Johana <johanatonguino94@gmail.com>
2022-10-20 11:45:18 +0200

    ejercicion botton creador eliminacion de datos de personas

--- src/MyUserInterface.java
+++ src/MyUserInterface.java
@@ -3,11 +3,11 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 import java.util.ArrayList;
-
+import java.util.Scanner;
 
 public class MyUserInterface extends JFrame {
-
 
     private JPanel mainPanel;
     private JTextField name1;
@@ -17,11 +17,16 @@ public class MyUserInterface extends JFrame {
     private JButton addUserButton;
     private JPanel tituloGeneral;
     private JLabel print;
-    ArrayList<Persona> peopleUsuare = new ArrayList<>(); // (variable global) lo ven todos y (variables locales)  debe estar dentro de un metodo y sera manejados solo los atributos de dicho metodo  ejemplo  MyUserInterface es un metodo mas si quieres que solo este metodo vea este array debes ponerlo dentro.
+    private JButton Save;
+    private JButton delete;
+    private JTextField deletePerson;
+    private JButton Delete;
+    static ArrayList<Persona> peopleUsuare = new ArrayList<>(); // (variable global) lo ven todos y (variables locales)  debe estar dentro de un metodo y sera manejados solo los atributos de dicho metodo  ejemplo  MyUserInterface es un metodo mas si quieres que solo este metodo vea este array debes ponerlo dentro.
+
+    private static JLabel print2;
 
     MyUserInterface() {
-
-
+        print2 = print;
         mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
@@ -29,10 +34,8 @@ public class MyUserInterface extends JFrame {
 
         addUserButton.addActionListener(new ActionListener() {
 
-
             @Override
             public void actionPerformed(ActionEvent e) {
-
 
                 String nameUsure = name1.getText();
                 String age = edad.getText();
@@ -55,23 +58,74 @@ public class MyUserInterface extends JFrame {
                             " Weight: " + weightC + "<br/>" +
                             " Autorized: " + autorizacionC + "<br/> " +
                             "________________" + "<br/>";
-
                     print.setText(general);
                 }
-
                 name1.setText("");
                 edad.setText("");
                 peso.setText("");
-
             }
 
         });
+        Save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    FileWriter fw = new FileWriter("C:\\Users\\Darwin\\IdeaProjects\\my-java-course\\saveArrays");
+                    String dataUser = fromArrayToString(peopleUsuare);
+
+                    fw.write(dataUser);
+                    fw.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        delete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String datoDelete = deletePerson.getText();
+
+                for (int i = 0; i < peopleUsuare.size(); i++) {
+                    if (peopleUsuare.get(i).name.startsWith(datoDelete) == true) {
+                        peopleUsuare.remove(i);
+                        printfArray();
+                    }
+                }
+            }
+        });
     };
+     static void printfArray (){
+         String imprimir = "";
+
+         for (int i = 0; i < peopleUsuare.size(); i++) {
+
+             imprimir = imprimir + peopleUsuare.get(i).toString();
+         }
+         print2.setText(imprimir);
+
+    }
 
 
     public static void main(String[] args) {
+
         JFrame frame = new MyUserInterface();
         frame.setVisible(true);
+
+        String data = "";
+        try {
+            File file = new File("saveArrays");
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                data += myReader.nextLine();
+            }
+            peopleUsuare = fromStringToArray(data);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        printfArray();
+
     }
 
     static String fromArrayToString(ArrayList<Persona> personas) {
@@ -91,7 +145,9 @@ public class MyUserInterface extends JFrame {
             }
         }
         fromStringToArray(general);
+
         return general;
+
     }
 
 
@@ -104,7 +160,7 @@ public class MyUserInterface extends JFrame {
 
         for (int i = 0; i < partes.length; i++) {
             finals = partes[i].split("&");
-            Persona user = new Persona(finals[0],Integer.parseInt(finals[1]),Float.parseFloat(finals[2]),Boolean.getBoolean(finals[3]));
+            Persona user = new Persona(finals[0], Integer.parseInt(finals[1]), Float.parseFloat(finals[2]), Boolean.getBoolean(finals[3]));
             nameUser.add(user);
         }
         return nameUser;
@@ -124,7 +180,16 @@ class Persona {
         this.weight = weight;
         this.autorizacion = autorizacion;
 
+    }
 
+    public String toString() {
+        //return "Name:" + name+" Edad:"+age+" Peso:"+weight+" Autorizacion:"+autorizacion;
+
+        return "<html> Name: " + name + "<br/>" +
+                " Age: " + age + "<br/>" +
+                " Weight: " + weight + "<br/>" +
+                " Autorized: " + autorizacion + "<br/> " +
+                "________________" + "<br/>";
     }
 }
 
