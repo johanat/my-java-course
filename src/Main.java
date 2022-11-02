d2a525e453d1b40a30f58b1e3c7a8330999a4070
Johana <johanatonguino94@gmail.com>
2022-11-02 16:49:46 +0100

    exercises finished

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        float note1,note2, note3, sum=0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite 3 notas");
+        note1=input.nextFloat();
+        note2= input.nextFloat();
+        note3= input.nextFloat();
+
+        sum=note1+note2+note3;
+        System.out.print(sum);
+
+
+
+
     }
 }
 
