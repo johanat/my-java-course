7bde33b32dcd3f8daf0e0d5076127e06527a73e8
Johana <johanatonguino94@gmail.com>
2022-11-02 16:07:32 +0100

    start point

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Person johana = new Person("Johana", 5);
-        Person darwin = new Person("Darwin", 35);
 
-        johana.eat();
     }
 }
 
-class Person {
-
-    String name;
-    int age;
-    float weight = 90; //Peso
-
-    Person(String paraName, int paramAge){
-        this.name = paraName;
-        this.age = paramAge;
-    }
-
-    void eat() {
-        System.out.println("Eating...");
-    }
-
-    void sleep() {
-        System.out.println("Sleeping...");
-    }
-
-    void printInfo() {
-        System.out.println("My name is " + name + ", I'm "+ age + ", and my weight is " + weight);
-    }
-}
