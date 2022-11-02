4f3dd5c70ee72fddacc7901ca84c39cd91a7041f
Johana <johanatonguino94@gmail.com>
2022-11-02 11:13:35 +0100

    ejercicio abre a otra interfaz terminado

--- src/MyUserInterface.java
+++ src/MyUserInterface.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -10,123 +8,66 @@ import java.util.Scanner;
 public class MyUserInterface extends JFrame {
 
     private JPanel mainPanel;
-    private JTextField name1;
-    private JTextField edad;
-    private JTextField peso;
-    private JRadioButton buttonAutorizacion;
     private JButton addUserButton;
-    private JPanel tituloGeneral;
     private JLabel print;
-    private JButton Save;
+    public JButton Save;
     private JButton delete;
-    private JTextField deleteAndUpDate;
-    private JButton Actualizar;
-    private JButton Delete;
-    static ArrayList<Persona> peopleUsuare = new ArrayList<>(); // (variable global) lo ven todos y (variables locales)  debe estar dentro de un metodo y sera manejados solo los atributos de dicho metodo  ejemplo  MyUserInterface es un metodo mas si quieres que solo este metodo vea este array debes ponerlo dentro.
-
-    private static JLabel print2;
+    public JTextField deleteAndUpDate;
+    private JButton updateButton;
+    static ArrayList<Persona> peopleUser = new ArrayList<>(); // (variable global) lo ven todos y (variables locales)  must be inside a method and  Only the attributes of said method will be handled. Example MyUserInterface is one more method if you want only this method to see this array you must put it inside.
+    public static JLabel print2;
 
     MyUserInterface() {
         print2 = print;
         mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
-        this.setSize(new Dimension(500, 600));
+        this.setSize(new Dimension(670, 600));
 
-        addUserButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                String nameUsure = name1.getText();
-                String age = edad.getText();
-                String weight = peso.getText();
-                boolean autorizacionR = buttonAutorizacion.isSelected();
-
-                Persona person = new Persona(name1.getText(), Integer.parseInt(age), Float.parseFloat(weight), buttonAutorizacion.isSelected());
-                peopleUsuare.add(person);
-
-                printfArray();
-
-                name1.setText("");
-                edad.setText("");
-                peso.setText("");
-            }
-
+        addUserButton.addActionListener(e -> {
+            ButtonOfCall viewPanel = new ButtonOfCall(deleteAndUpDate, false); // before the call to other class must  instantiate an object.
+            viewPanel.setVisible(true); // used to call another window or example class ButtonOfCall
         });
-        Save.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    FileWriter fw = new FileWriter("C:\\Users\\Darwin\\IdeaProjects\\my-java-course\\saveArrays");
-                    String dataUser = fromArrayToString(peopleUsuare);
+        Save.addActionListener(e -> {
+            try {
+                FileWriter fw = new FileWriter("C:\\Users\\Darwin\\IdeaProjects\\my-java-course\\saveArrays");
+                //  String dataUser = fromArrayToString(peopleUser);
+                String dataUser = fromArrayToString(peopleUser);
 
-                    fw.write(dataUser);
-                    fw.close();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                fw.write(dataUser);
+                fw.close();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         });
 
-        delete.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String datoDelete = deleteAndUpDate.getText();
-
-                for (int i = 0; i < peopleUsuare.size(); i++) {
-                    if (peopleUsuare.get(i).name.startsWith(datoDelete) == true) {
-                        peopleUsuare.remove(i);
-                        printfArray();
-                    }
+        delete.addActionListener(e -> {
+            String dataDelete = deleteAndUpDate.getText();
+            for (int i = 0; i < peopleUser.size(); i++) {
+                if (peopleUser.get(i).id == Long.parseLong(dataDelete)) {
+                    //noinspection SuspiciousListRemoveInLoop
+                    peopleUser.remove(i);
+                    printfArray();
                 }
             }
         });
-        Actualizar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String update = deleteAndUpDate.getText();
-                String generalActulizar="";
+        updateButton.addActionListener(e -> {
 
-
-
-
-                for (int i = 0; i < peopleUsuare.size(); i++) {
-                    if (peopleUsuare.get(i).name.startsWith(update) == true) {
-                        if(!name1.getText().equals("")){
-                            peopleUsuare.get(i).name = name1.getText();
-                        }
-                        if(!edad.getText().equals("")){
-                            peopleUsuare.get(i).age =  Integer.parseInt(edad.getText());
-                        }
-                        if(!peso.getText().equals("")){
-                            peopleUsuare.get(i).weight = Float.parseFloat(peso.getText());
-                        }
-                        peopleUsuare.get(i).autorizacion=buttonAutorizacion.isSelected();
-
-                    }
-
-                }
-                printfArray();
-
-
-            }
+            ButtonOfCall viewPanel = new ButtonOfCall(deleteAndUpDate, true);
+            viewPanel.setVisible(true);
         });
     }
-
-    ;
 
     static void printfArray() {
-        String imprimir = "";
+        String printfVariable = "";
 
-        for (int i = 0; i < peopleUsuare.size(); i++) {
+        for (Persona persona : peopleUser) {
 
-            imprimir = imprimir + peopleUsuare.get(i).toString();
+            //noinspection StringConcatenationInLoop
+            printfVariable += persona.toString();
         }
-        print2.setText(imprimir);
-
+        print2.setText(printfVariable);
     }
-
 
     public static void main(String[] args) {
 
@@ -138,18 +79,16 @@ public class MyUserInterface extends JFrame {
             File file = new File("saveArrays");
             Scanner myReader = new Scanner(file);
             while (myReader.hasNextLine()) {
+                //noinspection StringConcatenationInLoop
                 data += myReader.nextLine();
             }
-            peopleUsuare = fromStringToArray(data);
-
+            peopleUser = fromStringToArray(data);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
         printfArray();
-
     }
-
-    static String fromArrayToString(ArrayList<Persona> personas) {
+    String fromArrayToString(ArrayList<Persona> personas) {
         String general = "";
 
         int counter = personas.size() - 1;
@@ -158,59 +97,71 @@ public class MyUserInterface extends JFrame {
             String nameC = personas.get(i).name;
             String ageC = String.valueOf(personas.get(i).age);
             String weightC = Float.toString(personas.get(i).weight);
-            boolean autorizacionC = personas.get(i).autorizacion;
+            boolean authorization = personas.get(i).authorizationBoolean;
+            long millis = personas.get(i).id;
 
-            general += nameC + "&" + ageC + "&" + weightC + "&" + autorizacionC;
+            //noinspection StringConcatenationInLoop
+            general += (nameC + "&" + ageC + "&" + weightC + "&" + authorization + "&" + millis);
             if (i < counter) {
                 general += "##";
             }
         }
         fromStringToArray(general);
-
         return general;
-
     }
-
 
     static ArrayList<Persona> fromStringToArray(String general) {
 
-        ArrayList nameUser = new ArrayList<>();
+        ArrayList<Persona>nameUser = new ArrayList<>();
         String[] finals;
 
-        String[] partes = general.split("##");
+        String[] parts = general.split("##");
 
-        for (int i = 0; i < partes.length; i++) {
-            finals = partes[i].split("&");
-            Persona user = new Persona(finals[0], Integer.parseInt(finals[1]), Float.parseFloat(finals[2]), Boolean.getBoolean(finals[3]));
+        for (String parte : parts) {
+            finals = parte.split("&");
+            Persona user = new Persona(finals[0], Integer.parseInt(finals[1]), Float.parseFloat(finals[2]), Boolean.getBoolean(finals[3]), Long.parseLong(finals[4]));
             nameUser.add(user);
         }
+
         return nameUser;
     }
+
+    static long getId() {
+
+        long idMax = 0;
+
+
+        for (Persona persona : peopleUser) {
+            if (persona.id > idMax) {
+                idMax = persona.id;
+            }
+        }
+        return idMax + 1;
+    }
 }
+
 
 class Persona {
     String name;
     int age;
     float weight;
-    boolean autorizacion;
+    boolean authorizationBoolean;
+    long id;
 
-    Persona(String name, int age, float weight, boolean autorizacion) {
+    Persona(String name, int age, float weight, boolean authorization1, long id) {
 
         this.name = name;
         this.age = age;
         this.weight = weight;
-        this.autorizacion = autorizacion;
+        this.authorizationBoolean = authorization1;
+        this.id = id;
 
     }
 
     public String toString() {
-        //return "Name:" + name+" Edad:"+age+" Peso:"+weight+" Autorizacion:"+autorizacion;
 
-        return "<html> Name: " + name + "<br/>" +
-                " Age: " + age + "<br/>" +
-                " Weight: " + weight + "<br/>" +
-                " Autorized: " + autorizacion + "<br/> " +
-                "________________" + "<br/>";
+
+        return "<html> "+"Id: "+id+ "<br/>"+"Name: " + name + "<br/>" + " Age: " + age + "<br/>" + " Weight: " + weight + "<br/>" + " Authorized: " + authorizationBoolean + "<br/> " + "__________________________" + "<br/>";
     }
 }
 
