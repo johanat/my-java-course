13878d7f1774b9cb7f9bc8fa6cabe7f74b8da227
Johana <johanatonguino94@gmail.com>
2022-11-03 16:26:25 +0100

    ejercicio 3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-       Scanner input = new Scanner(System.in);
-       float hoursWeekly, hoursEveryday, salaryWeekly;
-        System.out.println("Introduce your working  hours weekly and the hour of work ");
-        hoursWeekly= input.nextFloat();
-        hoursEveryday = input.nextFloat();
-        salaryWeekly= hoursWeekly*hoursEveryday;
-        System.out.println("The salary weekly = "+salaryWeekly);
-        
+        float guillermo, luis, juan, total;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce the quantity of money that has Guillermo ");
+        guillermo= input.nextFloat();
+        luis= guillermo/2;
+        juan= (guillermo+luis)/2;
+        total=guillermo+luis+juan;
+        System.out.println("\nGuillermo: "+guillermo+"\n luis: "+luis+"\njuan. "+juan+ "\n\n la cantidad de dinero tiene juan es : "+total);
     }
 }
 
