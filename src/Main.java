66294a64dfafacd7734947461831c121143f7175
Johana <johanatonguino94@gmail.com>
2022-11-06 22:11:47 +0100

    ejercicio 8

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int hoursTotal, weeks, days, hours;
+        double a, b, c, result1,result2;
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce the Number of horas total ");
-        hoursTotal = input.nextInt();
-        weeks = hoursTotal / 168;
-        days = hoursTotal % 168 / 24;
-        hours = hoursTotal % 168 % 24;
-        System.out.println("\nsemanas= " + weeks + "\ndias= " + days + "\nhoras= " + hours);
+        System.out.println("Introduce the value of a");
+        a=input.nextDouble();
+        System.out.println("Introduce the value of b");
+        b=input.nextDouble();
+        System.out.println("Introduce the value of c");
+        c=input.nextDouble();
+
+        result1= (-b+Math.sqrt(Math.pow(b,2)-(4*a*c))) / (2*a);
+        result2= (-b-Math.sqrt(Math.pow(b,2)-(4*a*c))) /(2*a);
+        System.out.println("\nresultado1= "+result1);
+        System.out.println("\nresultado2= "+ result2);
     }
 }
 
