f2cb0eaa3648ab2221076a6351645a8690ceddb8
Johana <johanatonguino94@gmail.com>
2022-11-06 16:02:12 +0100

    ejercicio 7

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        float participation, firstExam, secondExam, examFinal, qualificationFinal;
+        int hoursTotal, weeks, days, hours;
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce the fort note of the student");
-        participation = input.nextFloat();
-        firstExam = input.nextFloat();
-        secondExam = input.nextFloat();
-        examFinal = input.nextFloat();
-
-        qualificationFinal= (participation*0.10f)+(firstExam*0.25f)+(secondExam*0.25f)+(examFinal*0.40f);
-        System.out.println("QualificationFinal is "+qualificationFinal);
-
+        System.out.println("Introduce the Number of horas total ");
+        hoursTotal = input.nextInt();
+        weeks = hoursTotal / 168;
+        days = hoursTotal % 168 / 24;
+        hours = hoursTotal % 168 % 24;
+        System.out.println("\nsemanas= " + weeks + "\ndias= " + days + "\nhoras= " + hours);
     }
 }
 
