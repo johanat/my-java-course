40f02e4fdfd4891214a6edfb6b781f9243284544
Johana <johanatonguino94@gmail.com>
2022-11-07 18:44:15 +0100

    conditional exercise 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int  number;
+        int num1, num2;
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce a number");
-        number= input.nextInt();
-        if( number%10==0){
-            System.out.println("Yes is multiple of 10  = "+number);
+        System.out.println("Introduce a num1");
+        num1=input.nextInt();
+        System.out.println("Introduce a num2");
+        num2=input.nextInt();
+        if(num1>num2){
+            System.out.println("the largest number is = "+num1);
+        }
+        else if(num2>num1){
+            System.out.println("The largest number is = "+ num2);
         }
         else{
-            System.out.println("Not is multiple of 10");
+            System.out.println("Are same");
         }
     }
 }
