7cd9b1fa854df0a16e17841128e68ee527cb3004
Johana <johanatonguino94@gmail.com>
2022-11-10 16:29:36 +0100

    Conditional exercise 9

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,30 @@
 
 
 import java.util.Scanner;
-// exercise N.8
+// exercise N.9
 
 public class Main {
     public static void main(String[] args) {
-        int num1;
+        int day, month, year;
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce a number ");
-        num1 = input.nextInt();
-
-        if (num1 < 10) {
-            System.out.println("This number has 1 cipher");
-        } else if (num1 < 100){
-            System.out.println("This number has 2 cipher");
-        }
-        else if (num1 < 1000){
-            System.out.println("This  number has 3 cipher");
-        }
-        else if(num1 <10000){
-            System.out.println("This number has 4 cipher");
-        }
-        else if(num1 < 100000){
-            System.out.println("This number has 5 cipher");
+        System.out.println("Introduce the day");
+        day = input.nextInt();
+        System.out.println("Introduce the month ");
+        month = input.nextInt();
+        System.out.println("Introduce the first number ");
+        year = input.nextInt();
+        if (day >= 1 && day <= 30) { // si el dia es correct
+            if (month >= 1 && month <= 12) { // si el mes es correct
+                if (year != 0) { // si el año es correct
+                    System.out.println("The data is correct");
+                }else{
+                    System.out.println("The year is incorrect");
+                }
+            }else{
+                System.out.println("The month is incorrect");
+            }
+        }else{
+            System.out.println("The day is correct");
         }
     }
 }
