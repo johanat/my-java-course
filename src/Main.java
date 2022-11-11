2089a43d60dc8c18a61e4b258aa358d3bd8957e9
Johana <johanatonguino94@gmail.com>
2022-11-11 17:18:10 +0100

    conditional exercise 11

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,35 @@
 
 
 import java.util.Scanner;
-// exercise N.10
+// exercise N.11
 
 public class Main {
     public static void main(String[] args) {
-        int day, month, year;
+        int num1, num2;
+        char operation;
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce the day");
-        day = input.nextInt();
-        System.out.println("Introduce the month ");
-        month = input.nextInt();
-        System.out.println("Introduce the year ");
-        year = input.nextInt();
-
-        if(day>=1 && day<=31 && month==1 && year !=0) {
-            System.out.println("The date is correct");
-
-        }
-        else if(day>=1 && day<=28 && month==2 && year !=0){
-            System.out.println("The date is correct");
+        System.out.println("Introduce the num1");
+        num1= input.nextInt();
+        System.out.println("Introduce the num2");
+        num2= input.nextInt();
+        System.out.println("Introduce the operation that  want realise");
+        operation= input.next().charAt(0);
+        if(operation=='s' || operation=='S'){
+            System.out.println("The sum is "+(num1+num2));
 
         }
-        else if (day>=1 && day<=31 && month ==3 && year !=0) {
-            System.out.println("The date is correct");
-
+        else if(operation=='r' || operation=='R'){
+            System.out.println("The rest is " +(num1-num2));
         }
-        else if( day>=1 && day<=30 && month ==4 && year !=0){
-            System.out.println("The date is correct");
-
+        else if(operation=='p' || operation=='P' || operation=='m' || operation=='M'){
+            System.out.println("The multiplication  is" +(num1*num2));
         }
-        else if (day>=1 && day<=31 && month ==5 && year !=0){
-            System.out.println("The date is correct");
-
-        }
-        else if (day>=1 && day<=30 && month ==6 && year !=0){
-            System.out.println("The date is correct");
-
-        }
-        else if(day>=1 && day<=31 && month == 7 && year !=0){
-            System.out.println("The date is correct");
-
-        }
-        else if(day>=1 && day<=31 && month == 8 && year !=0){
-            System.out.println("The date is correct");
-
-        }
-        else if(day>=1 && day<=30 && month == 9 && year !=0){
-            System.out.println("The date is correct");
-
-        }
-        else if (day>=1 && day<=31 && month == 10 && year !=0){
-            System.out.println("The date is correct");
-
-        }
-        else if ( day>=1 && day<=30 && month == 11 && year !=0){
-            System.out.println("The date is correct");
-
-        }
-        else if ( day>=1 && day <= 31 && month == 12 && year !=0){
-            System.out.println("The date is correct");
+        else if (operation== 'd' || operation== 'D'){
+            System.out.println("The division is "+(num1/num2));
 
         }
         else {
-            System.out.println("The date is incorrect");
+            System.out.println("The letter is incorrect");
         }
     }
 }
