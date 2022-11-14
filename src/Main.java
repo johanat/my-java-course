20d2dd5777074b3abec92caca66553bc6101c196
Johana <johanatonguino94@gmail.com>
2022-11-14 02:09:11 +0100

    conditional exercise 13

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,38 @@
 
 
 import java.util.Scanner;
-// exercise N.12
+// exercise N.13
 
 public class Main {
     public static void main(String[] args) {
-        int note;
+        final int amount = 1000;
+        int option;
+        float inputMoney, exitMoney;
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce a note");
-        note = input.nextInt();
-        switch (note){
-            case 0:
+        System.out.println("\nwelcome to your ATM \n1.deposit money into the account\n" +
+                "2.withdraw money to the account\n" +
+                "3.exit ");
+        option = input.nextInt();
+        switch (option) {
             case 1:
+                System.out.println("Write the amount ");
+                inputMoney = input.nextFloat();
+                System.out.println("the total of your account is " + (amount + inputMoney));
+                break;
             case 2:
+
+                System.out.println("Write the amount");
+                exitMoney = input.nextFloat();
+                if (exitMoney > amount) {
+                    System.out.println("Your Amount is  insufficient");
+                } else {
+                    System.out.println("the total of your account is " + (amount - exitMoney));
+                }
+                break;
             case 3:
-            case 4:
-                System.out.println("The note is insufficient ");
+            default:
+                System.out.println("wrong menu option");
                 break;
-            case 5:
-            case 6:
-                System.out.println("The note is enough");
-                break;
-            case 7:
-            case 8:
-                System.out.println("The note is well remarkable");
-                break;
-            case 9:
-            case 10:
-                System.out.println("The note is well outstanding");
-                break;
-            default:System.out.println("The note is incorrect");
         }
     }
 }
