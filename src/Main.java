2a39b79cd8649fa58e2bb2114911bc6f8ada8a83
Johana <johanatonguino94@gmail.com>
2022-11-14 01:20:28 +0100

    conditional exercise 12

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,35 @@
 
 
 import java.util.Scanner;
-// exercise N.11
+// exercise N.12
 
 public class Main {
     public static void main(String[] args) {
-        int num1, num2;
-        char operation;
+        int note;
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce the num1");
-        num1= input.nextInt();
-        System.out.println("Introduce the num2");
-        num2= input.nextInt();
-        System.out.println("Introduce the operation that  want realise");
-        operation= input.next().charAt(0);
-        if(operation=='s' || operation=='S'){
-            System.out.println("The sum is "+(num1+num2));
-
-        }
-        else if(operation=='r' || operation=='R'){
-            System.out.println("The rest is " +(num1-num2));
-        }
-        else if(operation=='p' || operation=='P' || operation=='m' || operation=='M'){
-            System.out.println("The multiplication  is" +(num1*num2));
-        }
-        else if (operation== 'd' || operation== 'D'){
-            System.out.println("The division is "+(num1/num2));
-
-        }
-        else {
-            System.out.println("The letter is incorrect");
+        System.out.println("Introduce a note");
+        note = input.nextInt();
+        switch (note){
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("The note is insufficient ");
+                break;
+            case 5:
+            case 6:
+                System.out.println("The note is enough");
+                break;
+            case 7:
+            case 8:
+                System.out.println("The note is well remarkable");
+                break;
+            case 9:
+            case 10:
+                System.out.println("The note is well outstanding");
+                break;
+            default:System.out.println("The note is incorrect");
         }
     }
 }
