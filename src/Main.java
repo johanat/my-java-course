ebe3c4876877ef469c169f62979f29566d93eba4
Johana <johanatonguino94@gmail.com>
2022-11-14 02:54:15 +0100

    conditional exercise 14

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,53 @@
 
 
 import java.util.Scanner;
-// exercise N.13
+// exercise N.14
 
 public class Main {
     public static void main(String[] args) {
-        final int amount = 1000;
+
         int option;
-        float inputMoney, exitMoney;
+        float measure = 0;
         Scanner input = new Scanner(System.in);
-        System.out.println("\nwelcome to your ATM \n1.deposit money into the account\n" +
-                "2.withdraw money to the account\n" +
-                "3.exit ");
+        System.out.println("Digit la amount of measure");
+        measure = input.nextFloat();
+        System.out.println("\nMenu " +
+                "\n 1.Hectogramos" +
+                "\n 2.Decagramo" +
+                "\n 3.Gramo+" +
+                "\n 4.Decigramo" +
+                "\n 5.Centigramo" +
+                "\n 6.Miligramo");
         option = input.nextInt();
+
         switch (option) {
             case 1:
-                System.out.println("Write the amount ");
-                inputMoney = input.nextFloat();
-                System.out.println("the total of your account is " + (amount + inputMoney));
+                measure *= 10;
+                System.out.println("The measure in Hectogramos " + measure);
                 break;
             case 2:
-
-                System.out.println("Write the amount");
-                exitMoney = input.nextFloat();
-                if (exitMoney > amount) {
-                    System.out.println("Your Amount is  insufficient");
-                } else {
-                    System.out.println("the total of your account is " + (amount - exitMoney));
-                }
+                measure *= 100;
+                System.out.println("The measure in Decagramo" + measure);
                 break;
             case 3:
-            default:
-                System.out.println("wrong menu option");
+                measure *= 1000;
+                System.out.println("The measure in Gramo " + measure);
                 break;
+            case 4:
+                measure *= 10000;
+                System.out.println("The measure in Decigramo " + measure);
+                break;
+            case 5:
+                measure *= 100000;
+                System.out.println("The measure in Centigramo " + measure);
+                break;
+            case 6:
+                measure *= 1000000;
+                System.out.println("The measure in Miligramo " + measure);
+                break;
+            default:
+                System.out.println(" Option incorrect");
+
         }
     }
 }
