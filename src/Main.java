a48065e14ab28cb08bcabf6d8557a15f2d90a5f4
Johana <johanatonguino94@gmail.com>
2022-11-15 15:59:09 +0100

    bucles exercises N.3

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
 
 
 import java.util.Scanner;
-// exercise bucles N.2
+// exercise bucles N.3
 
 public class Main {
     public static void main(String[] args) {
 
-        int num, result;
-
+        int num;
         Scanner input = new Scanner(System.in);
         System.out.println("Introduce a number ");
-        num=input.nextInt();
+        num = input.nextInt();
 
         while(num!=0){
-            if(num>0){
-                System.out.println("The number is positive (+)");
+            if(num%2==0){
+                System.out.println("The number "+num+ " is pare ");
             }
             else{
-                System.out.println("The number is negative (-)");
+                System.out.println("The number "+ num+ " is odd");
             }
             System.out.println("Introduce a number ");
             num=input.nextInt();
