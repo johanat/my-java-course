d9a3ce3617122214f4c770f62a60524f06f9b3e9
Johana <johanatonguino94@gmail.com>
2022-11-16 16:49:01 +0100

    Bucles exercise N.5

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,36 @@
 
 
 import java.util.Scanner;
-// exercise bucles N.4
+// exercise bucles N.5
 
 public class Main {
     public static void main(String[] args) {
 
-        int num,i ;
+        int num, round, counter=0;
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce a number ");
+        System.out.println("Introduce a number");
         num = input.nextInt();
-       for( i=0; num>=0; i++){
-           System.out.println("Introduce a number ");
-           num = input.nextInt();
-       }
-        System.out.println("Number introduced are "+i + " Not negative");
+        round = (int) (Math.random() * 100);
+
+
+        do {
+            if (round > num) {
+                System.out.println("The number is major ");
+                System.out.println("Introduce a number");
+                num= input.nextInt();
+            }
+            else {
+                System.out.println("The number is minor  ");
+                System.out.println("Introduce a number");
+                num= input.nextInt();
+            }
+            counter++;
+
+        } while (num != round);
+        System.out.println("Cool you won,you guessed the number in  "+counter+ " try");
+        num=input.nextInt();
+
     }
 }
+
 
