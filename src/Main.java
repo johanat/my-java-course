389fbbc58a634dbfbc5662701f1cf1afa0c5ac82
Johana <johanatonguino94@gmail.com>
2022-11-22 00:47:34 +0100

    bucles exercise N.11

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,20 @@
 
-
-import java.security.spec.RSAOtherPrimeInfo;
-import java.util.Scanner;
-// exercise bucles N.10
+// exercise bucle N.11
 
 public class Main {
     public static void main(String[] args) {
 
 
-        int num, suma=0;
-        Scanner input = new Scanner(System.in);
+        long multiplication=1;
 
-        for( int i=1; i<=10; i++){
-            System.out.println("Introduce a number");
-            num= input.nextInt();
-            suma+=num;
 
+        for (int i = 1; i <= 20; i+=2) {
+            multiplication*=i;
+            }
+            System.out.println(multiplication);
         }
-        System.out.println("The sum = "+suma);
-
     }
-}
+
 
 
 
