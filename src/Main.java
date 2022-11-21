c9b6db0fd616ca492c9bf1e51d1ea2c8127fc074
Johana <johanatonguino94@gmail.com>
2022-11-21 17:22:43 +0100

    bucles exercise N.7

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,35 @@
 
 
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
-// exercise bucles N.6
+// exercise bucles N.7
 
 public class Main {
     public static void main(String[] args) {
 
-        int num, suma;
-        Scanner input= new Scanner(System.in);
+        int num, suma = 0, counter = 1;
+        float media = 0;
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Introduce a number");
-        num= input.nextInt();
-        suma=num;
-        while(num!=0){
-            System.out.println("introduce a number");
-            num= input.nextInt();
-            suma+=num;
+        num = input.nextInt();
+
+        if (num >= 0) {
+            suma += num;
         }
-        System.out.println("sum of all the number are : "+ suma);
+        while (num >= 0) {
+            System.out.println("Introduce a number");
+            num = input.nextInt();
+            if (num >= 0) {
+                suma += num;
+                counter++;
+                media = (float)suma / counter;
+            }
+        }
+        System.out.println("La media es " + media);
     }
 }
+
 
 
