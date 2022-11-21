f8b38a36e64c46eece58bbd50403079282cf7b82
Johana <johanatonguino94@gmail.com>
2022-11-22 00:07:49 +0100

    exercise N.10

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,23 @@
 
 import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
-// exercise bucles N.9
+// exercise bucles N.10
 
 public class Main {
     public static void main(String[] args) {
 
 
-        int num = 0;
+        int num, suma=0;
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 100; i >= 0; i -= 7) {
-            System.out.println(i);
+        for( int i=1; i<=10; i++){
+            System.out.println("Introduce a number");
+            num= input.nextInt();
+            suma+=num;
+
         }
+        System.out.println("The sum = "+suma);
+
     }
 }
 
