b12df6bbc40b8888ecca6e087e39951c881db229
Johana <johanatonguino94@gmail.com>
2022-11-21 18:55:02 +0100

    bucles exercise N.9

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,17 @@
 
 import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
-// exercise bucles N.8
+// exercise bucles N.9
 
 public class Main {
     public static void main(String[] args) {
 
-      Scanner input =  new Scanner(System.in);
-        int num;
-        System.out.println("Introduce a number ");
-        num= input.nextInt();
 
-       for ( int i=1; i<=num; i++){
-           System.out.println(i);
-       }
+        int num = 0;
+
+        for (int i = 100; i >= 0; i -= 7) {
+            System.out.println(i);
+        }
     }
 }
 
