641ffa7738821c6396b248ac6bf0dd1f1b6e7403
Johana <johanatonguino94@gmail.com>
2022-11-22 17:33:37 +0100

    bucles N.13

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,44 @@
-
-// exercise bucle N.12
+// exercise bucle N.13
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int num;
-        long factorial=1;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Introduce a number ");
-        num= input.nextInt();
-        for( int i=1; i<=num;  i++  ){
-            factorial*=i;
-        }
-        System.out.println("The factorial of "+ num + " is "+factorial );
+        int num, sumaNumberPositive = 0, sumaNumberNegative = 0, counterP = 0, counterN = 0, counterZero = 0;
+        float mediaP = 0, mediaN = 0;
 
+
+        Scanner input = new Scanner(System.in);
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Introduce a number " + i);
+            num = input.nextInt();
+
+            if (num == 0) {
+                counterZero++;
+            } else if (num > 0) {
+                sumaNumberPositive += num;
+                counterP++;
+            } else {
+                sumaNumberNegative += num;
+                counterN++;
+            }
         }
+        if (counterP == 0) {
+            System.out.println(" Can't  remove media, there is negative numbers ");
+        } else {
+            mediaP = (float) sumaNumberPositive / counterP;
+            System.out.println("The media of the numbers positive is " + mediaP);
+        }
+        if (counterN == 0) {
+            System.out.println("Can't remove media, There is negative numbers ");
+        } else {
+            mediaN = (float) sumaNumberNegative / counterN;
+            System.out.println("The media of the numbers positive is " + mediaN);
+        }
+        System.out.println("The amount of is " + counterZero);
     }
+}
 
 
 
