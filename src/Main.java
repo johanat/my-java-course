3ffda8f6b09b06bd144d9ed9be4d75175bbaf61b
Johana <johanatonguino94@gmail.com>
2022-11-22 01:16:09 +0100

    bucles exercise N.12

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 
-// exercise bucle N.11
+// exercise bucle N.12
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int num;
+        long factorial=1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce a number ");
+        num= input.nextInt();
+        for( int i=1; i<=num;  i++  ){
+            factorial*=i;
+        }
+        System.out.println("The factorial of "+ num + " is "+factorial );
 
-
-        long multiplication=1;
-
-
-        for (int i = 1; i <= 20; i+=2) {
-            multiplication*=i;
-            }
-            System.out.println(multiplication);
         }
     }
 
