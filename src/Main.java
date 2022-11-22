4cbbae84acbe4377775342ad679647dc286c831d
Johana <johanatonguino94@gmail.com>
2022-11-22 18:08:22 +0100

    bucles exercises  N.14

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,25 @@
-// exercise bucle N.13
+// exercise bucle N.14
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int num, sumaNumberPositive = 0, sumaNumberNegative = 0, counterP = 0, counterN = 0, counterZero = 0;
-        float mediaP = 0, mediaN = 0;
+        float salary, sumSalary = 0;
+        int counter = 0;
 
 
         Scanner input = new Scanner(System.in);
-        
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("Introduce a number " + i);
-            num = input.nextInt();
 
-            if (num == 0) {
-                counterZero++;
-            } else if (num > 0) {
-                sumaNumberPositive += num;
-                counterP++;
-            } else {
-                sumaNumberNegative += num;
-                counterN++;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Introduce the salary " + i);
+            salary = input.nextFloat();
+            sumSalary += salary;
+            if (salary > 1000) {
+                counter++;
             }
         }
-        if (counterP == 0) {
-            System.out.println(" Can't  remove media, there is negative numbers ");
-        } else {
-            mediaP = (float) sumaNumberPositive / counterP;
-            System.out.println("The media of the numbers positive is " + mediaP);
-        }
-        if (counterN == 0) {
-            System.out.println("Can't remove media, There is negative numbers ");
-        } else {
-            mediaN = (float) sumaNumberNegative / counterN;
-            System.out.println("The media of the numbers positive is " + mediaN);
-        }
-        System.out.println("The amount of is " + counterZero);
+        System.out.println("The sum of all the salary is " + sumSalary);
+        System.out.println(" total major to 1000 = " + counter);
     }
 }
 
