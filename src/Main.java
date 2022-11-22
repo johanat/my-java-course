e6a8af40adb64dd11b556ba65f72bf749ea72a4c
Johana <johanatonguino94@gmail.com>
2022-11-23 00:10:19 +0100

    bucle exercise N.16

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,26 @@
-// exercise bucle N.15
+// exercise bucle N.16
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int  ages, counterAges =0,counterHeights=0, sumaOfAges=0;
-        float sumaOfHeights=0,mediaAges=0, mediaHeights=0, heights;
+        int num, multiplication;
         Scanner input = new Scanner(System.in);
-        for(int i=0; i<5; i++){
-            System.out.println("Introduce the age of the student ");
-            ages=input.nextInt();
-            sumaOfAges+=ages;
-            mediaAges=(float) sumaOfAges/5;
-            if(ages>18){
-                counterAges++;
-            }
+
+        do {
+            System.out.println("Introduce a number between 0 and 10");
+            num = input.nextInt();
+
+        } while (num < 0 || num > 10);
+
+        System.out.println("The table of multiplication is\n");
+        for (int i = 0; i < 10; i++) {
+            multiplication = num * i;
+            System.out.println(num + "*" + i + "=" + multiplication);
         }
-        System.out.println("The media of ages is "+ mediaAges);
-        System.out.println("Total number of students over 18 years is "+counterAges );
-
-        for(int j=0; j<5; j++){
-            System.out.println("Introduce the heights of the students");
-            heights=input.nextFloat();
-            sumaOfHeights+=heights;
-            mediaHeights= (float) Math.round(100*sumaOfHeights/5)/100;
-
-            if(heights>1.75){
-                counterHeights++;
-            }
-        }
-        System.out.println("the media of heights is "+ mediaHeights);
-        System.out.println("Total numbers of students who measure more than  "+counterHeights );
-
-
     }
 }
+
 
 
 
