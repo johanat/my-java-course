5af46bb4ce5e94d3980cdce896b0595f5433a83d
Johana <johanatonguino94@gmail.com>
2022-11-23 19:00:05 +0100

    bucles exercise N.21

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,32 @@
-// exercise bucle N.20
+// exercise bucle N.21
+
 
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        int num, totalSalary, salary = 0, maxime=0;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Introduce a number");
-        num = input.nextInt();
-        for (int i = 0; i < num; i++) {
-            System.out.println("Introduce a salary");
-            salary = input.nextInt();
 
-            if (salary>maxime) {
-                maxime = salary;
+        Scanner input = new Scanner(System.in);
+        int num;
+        boolean thereIsNegative= false;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Introduce a number");
+            num=input.nextInt();
+            if(num<0){
+                thereIsNegative=true;
             }
         }
-        System.out.println("the salary maxime is " +maxime);
-
-
+            if(thereIsNegative==true){
+                System.out.println("exist  negative numbers ");
+            }
+            else{
+                System.out.println("it doesn't negative numbers");
+            }
+        }
     }
-}
+
 
 
 
