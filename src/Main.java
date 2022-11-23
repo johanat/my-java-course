21f3961cc3d2e5794c69a58c789ba799314b2f2b
Johana <johanatonguino94@gmail.com>
2022-11-23 02:17:29 +0100

    bucles exercise N.17

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
-// exercise bucle N.16
+// exercise bucle N.17
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int num, multiplication;
+        int articleCode, cantidadlitros=0, litros, litersTotal=0, counter=0;
+        float  precioLitro, billTotal=0;
         Scanner input = new Scanner(System.in);
+        for (int i=0; i<5; i++){
+            System.out.println("Introduce code of the bill");
+            articleCode=input.nextInt();
+            System.out.println("Introduce  sold amount liter ");
+            litros=input.nextInt();
+            System.out.println("Introduce the price of the liter  ");
+            precioLitro= input.nextFloat();
+            billTotal+=(float)cantidadlitros*precioLitro;
 
-        do {
-            System.out.println("Introduce a number between 0 and 10");
-            num = input.nextInt();
-
-        } while (num < 0 || num > 10);
-
-        System.out.println("The table of multiplication is\n");
-        for (int i = 0; i < 10; i++) {
-            multiplication = num * i;
-            System.out.println(num + "*" + i + "=" + multiplication);
+            if(articleCode==1){
+                cantidadlitros+=litros;
+            }
+            if(cantidadlitros*precioLitro>600){
+                counter++;
+            }
         }
+        System.out.println("billing total is "+ billTotal);
+        System.out.println("Amount total in liters of the article sold "+ cantidadlitros);
+        System.out.println("How many invoices were issued greater than 600 "+counter);
     }
 }
+
 
 
 
