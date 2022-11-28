07bd3667b818262e1511779f5ef306956fa6be47
Johana <johanatonguino94@gmail.com>
2022-11-28 17:02:00 +0100

    Vectores exercise N.1

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,16 @@
-// exercise bucle N.22
-
-
+// exercise vectors o array N.1
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int note, counter = 0;
-        boolean thereIsNotes = false;
-        Scanner input = new Scanner(System.in);
-
-        for (int i = 0; i < 5; i++) {
-            do {
-                System.out.println("Introduce a note");
-                note = input.nextInt();
-            }
-            while (note > 10 || note < 0);
-            if (note < 5) {
-                thereIsNotes = true;
-            }
+       Scanner input=new Scanner(System.in);
+       float num[]=new float[5];
+        for( int i=0; i<5; i++){
+            System.out.println(i+1+" Introduce a number");
+            num[i]=input.nextFloat();
         }
-        if (thereIsNotes == true) {
-            System.out.println("There are suspense");
-        } else {
-            System.out.println("there is no suspense ");
+        for(float i:num){
+            System.out.println(i);
         }
     }
 }
