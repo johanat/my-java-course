20c728e084fc8ac7469ee366cf0814714a0e7ac3
Johana <johanatonguino94@gmail.com>
2022-11-30 16:26:11 +0100

    Vectores o array exercise N.6

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,43 @@
-// exercise vectors o array N.5
+// exercise vectors o array N.6S
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int a[] , b[], c[];
-        a=new int[10];
-        b=new int [10];
-        c=new int[20];
+        a=new int[12];
+        b=new int [12];
+        c=new int[24];
 
         System.out.println("Fill the table A");
-        for(int i=0; i<10; i++){
+        for(int i=0; i<12; i++){
             System.out.print((i+1)+" Introduce a number ");
             a[i]=input.nextInt();
         }
         System.out.println("\n Fill the table B");
 
-        for(int k=0; k<10; k++){
+        for(int k=0; k<12; k++){
             System.out.print((k+1)+" Introduce a number ");
             b[k]=input.nextInt();
         }
         int j=0;
-        for(int i=0; i<10; i++ ){
+        for(int i=0; i<4; i++ ){
             c[j]=a[i];
+            j++;
+            c[j]=a[i];
+            j++;
+            c[j]=a[i];
+            j++;
+            c[j]=b[i];
+            j++;
+            c[j]=b[i];
             j++;
             c[j]=b[i];
             j++;
         }
         j=0;
         System.out.println("Result of the table C is");
-        for( int i=0; i<20; i++){
+        for( int i=0; i<24; i++){
             System.out.print(c[i]+" ");
         }
     }
