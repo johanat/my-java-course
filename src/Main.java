979b72bd0ac13db32d8fe6f1b0801a1eb9a059fb
Johana <johanatonguino94@gmail.com>
2022-12-01 17:46:33 +0100

    Vectores o array exercise N.10

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-// exercise vectors o array N.9
+// exercise vectors o array N.10
 
 import java.util.Scanner;
 
@@ -6,20 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numbers[];
-        int ultimo;
+        int ultimo,index ;
         numbers = new int[10];
 
         for (int i = 0; i < 10; i++) {
             System.out.print((i + 1) + " Introduce a number ");
             numbers[i] = input.nextInt();
         }
-        ultimo = numbers[9];
+        System.out.println("Enter the number deposition to displace ");
+        index=input.nextInt();
 
-        for (int i = 8; i >= 0; i--) {
-            numbers[i + 1] = numbers[i];
-        }
-        numbers[0] = ultimo;
-
+       for(int i=0; i<=index; i++){
+           ultimo=numbers[9];
+           for(int j=8; j>=0; j--){
+               numbers[j+1]=numbers[j];
+           }
+           numbers[0]=ultimo;
+       }
         for (int i = 0; i < 10; i++) {
             System.out.print(numbers[i] + " ");
         }
