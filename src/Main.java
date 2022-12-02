325ec7090840aac996b438554b4b882179c06bad
Johana <johanatonguino94@gmail.com>
2022-12-02 18:03:51 +0100

    Vector o array exercise n.11

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,47 @@
 // exercise vectors o array N.10
 
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numbers[];
-        int ultimo,index ;
+        int num, index=0;
         numbers = new int[10];
+        boolean creciente = false;
+        
+        do {
+            for (int i = 0; i < 5; i++) {
+                System.out.print((i + 1) + " Introduce a number ");
+                numbers[i] = input.nextInt();
+            }
+            for (int i = 0; i < 4; i++) {
+                if (numbers[i] < numbers[i + 1]) {
+                    creciente = true;
+                }
+                if (numbers[i] > numbers[i + 1]) {
+                    break;
+                }
+            }
+            if (creciente = false) {
+                System.out.println("The number are disordered");
+            }
+        } while (creciente = false);
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print((i + 1) + " Introduce a number ");
-            numbers[i] = input.nextInt();
+        System.out.println("Introduce a number ");
+        num = input.nextInt();
+
+        for(int i=0; numbers[i]<num && num<5; i++){
+            index++;
         }
-        System.out.println("Enter the number deposition to displace ");
-        index=input.nextInt();
-
-       for(int i=0; i<=index; i++){
-           ultimo=numbers[9];
-           for(int j=8; j>=0; j--){
-               numbers[j+1]=numbers[j];
-           }
-           numbers[0]=ultimo;
-       }
-        for (int i = 0; i < 10; i++) {
-            System.out.print(numbers[i] + " ");
+        for(int i=4; i>=index; i--){
+            numbers[i+1]=numbers[i];
+        }
+        numbers[index]=num;
+        System.out.println("the arrangement is as follows ");
+        for(int i=0; i<6; i++){
+            System.out.print(numbers[i]+" ");
         }
     }
 }
