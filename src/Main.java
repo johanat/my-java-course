939405f784f9aa7b780fcd4e9e19960e1774ba8d
Johana <johanatonguino94@gmail.com>
2022-12-09 10:51:00 +0100

    vectores o array exercise N.15

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,52 @@
-// exercise vectors o array N.14
+// exercise vectors o array N.15
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int a[] = new int[10], b[] = new int[10], c[] = new int[20];
-        boolean growing = true;
+        int num[] = new int[10];
+        boolean crecient = true;
+        int number;
+        boolean numberFound=false;
 
-        System.out.println("Introduce the first array");
-
-        do {
-            for (int i = 0; i < 10; i++) {
-                System.out.print((i + 1) + " Introduce a number");
-                a[i] = input.nextInt();
-            }
-            for (int i = 0; i < 9; i++) {
-                if (a[i] > i + 1) {
-                    growing = true;
-                }
-                if (a[i] < i + 1) {
-                    growing = false;
-                    break;
-                }
-            }
-            if (growing == false) {
-                System.out.println("The array is disordered");
-            }
-        } while (growing == false);
-
-        System.out.println("Introduce the second array ");
-
-        do {
-            for (int k = 0; k < 10; k++) {
-                System.out.print((k + 1) + "Introduce a number ");
-                b[k] = input.nextInt();
-            }
-            for (int j = 0; j < 9; j++) {
-                if (b[j] > j + 1) {
-                    growing = true;
-                }
-                if (b[j] < j + 1) {
-                    growing = false;
-                    break;
-                }
-            }
-            if (growing == false) {
-                System.out.println("The number is disordered");
-            }
-
-        } while (growing == false);
-
-        int i = 0, j = 0, k = 0;
-
-        while (i < 10 && j < 10) {
-
-            if (a[i] < b[j]) {
-                c[k] = a[i];
-                i++;
+        for (int i = 0; i < 10; i++) {
+            System.out.print((i ) + ". Introduce a number");
+            num[i] = input.nextInt();
+        }
+        for (int i = 0; i < 9; i++) {
+            if (num[i] < num[i + 1]) {
+                crecient = true;
             } else {
-                c[k] = b[j];
-                j++;
+                crecient = false;
+                break;
             }
-            k++;
         }
 
-        if (i == 10) {
-            while (j < 10) {
-                c[k] = b[j];
-                k++;
-                j++;
-            }
-        } else {
-            while (j == 10) {
-                c[k] = a[i];
-                k++;
-                i++;
+        if (crecient == false) {
+            for (int k = 0; k < 10; k++) {
+                System.out.println("\n Please try again");
+                System.out.print((k + 1) + ". Introduce a number");
+                num[k] = input.nextInt();
             }
         }
-        System.out.println("The array finaly is ");
-        for (int p = 0; p < 20; p++) {
-            System.out.print(c[p] + " -");
+        System.out.println("Introduce a number");
+        number = input.nextInt();
+
+        for (int i = 0; i < 10; i++) {
+            if(num[i]==number){
+                numberFound=true;
+                System.out.println("The position is "+ i );
+                break;
+            }
+            else{
+                numberFound=false;
+            }
         }
+        if(numberFound==false){
+            System.out.println("The position not is found");
+        }
+
     }
 }
 
