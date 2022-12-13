b4b9a5645fac83721b7c776487556e5a78b42635
Johana <johanatonguino94@gmail.com>
2022-12-13 11:25:39 +0100

    Vectores o array exercise N.16

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,61 @@
-// exercise vectors o array N.15
+// exercise vectors o array N.16
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num[] = new int[10];
-        boolean crecient = true;
-        int number;
-        boolean numberFound=false;
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print((i ) + ". Introduce a number");
-            num[i] = input.nextInt();
-        }
-        for (int i = 0; i < 9; i++) {
-            if (num[i] < num[i + 1]) {
-                crecient = true;
-            } else {
-                crecient = false;
-                break;
-            }
+        int suma1 = 0, suma2 = 0, suma3 = 0, num;
+        float first[] = new float[5], second[] = new float[5], third[] = new float[5], sumaStudent = 0, mediaStudent = 0, media1 = 0, media2 = 0, media3 = 0;
+        boolean major10 = false;
+
+        System.out.println(" \nNote date of the first trimester");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Note of the " + (i + 1) + " student ");
+            first[i] = input.nextInt();
+            suma1 += first[i];
+
         }
 
-        if (crecient == false) {
-            for (int k = 0; k < 10; k++) {
-                System.out.println("\n Please try again");
-                System.out.print((k + 1) + ". Introduce a number");
-                num[k] = input.nextInt();
-            }
-        }
-        System.out.println("Introduce a number");
-        number = input.nextInt();
+        media1 = suma1 / 5f;
 
-        for (int i = 0; i < 10; i++) {
-            if(num[i]==number){
-                numberFound=true;
-                System.out.println("The position is "+ i );
-                break;
-            }
-            else{
-                numberFound=false;
-            }
-        }
-        if(numberFound==false){
-            System.out.println("The position not is found");
+        System.out.println("\n Note date of the second trimester");
+        for (int i = 0;
+             i < 5; i++) {
+            System.out.print("Note of the " + (i + 1) + " student ");
+            second[i] = input.nextInt();
+            suma2 += second[i];
         }
 
+        media2 = suma2 / 5f;
+
+        System.out.println("\nNote date of the third trimester");
+        for (
+                int i = 0;
+                i < 5; i++) {
+            System.out.print("Note of the " + (i + 1) + " student ");
+            third[i] = input.nextInt();
+            suma3 += third[i];
+        }
+        media3 = suma3 / 5f;
+
+        System.out.println("\nMedia of the first trimester= " + media1);
+        System.out.println("Media of the second trimester = " + media2);
+        System.out.println("Media of the third trimester = " + media3);
+
+        System.out.println("\n Introduce the position ");
+        num = input.nextInt();
+
+        if (num > 5) {
+            System.out.println("Try agan");
+        }
+
+        sumaStudent += first[num] + second[num] + third[num];
+        
+        mediaStudent = sumaStudent / 3f;
+
+        System.out.println("Media of the position student is  " + mediaStudent);
     }
 }
 
