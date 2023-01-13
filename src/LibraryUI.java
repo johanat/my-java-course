1d034f8ad3c7022c9a6bc8755f5b412259b7b58a
Johana <johanatonguino94@gmail.com>
2023-01-13 18:14:25 +0100

    Basic interface implemented

--- src/LibraryUI.java
+++ src/LibraryUI.java
@@ -5,6 +5,16 @@ import java.awt.*;
 public class LibraryUI extends JFrame {
 
     private JPanel mainPanel;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JPanel panel3;
+    private JPanel panel4;
+    private JPanel panel5;
+    private JTextField myTitle;
+    private JTextField myAuthor;
+    private JTextField myYearOfPublish;
+    private JTextField numPages;
+    private JButton addBook;
 
     LibraryUI() {
         mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
